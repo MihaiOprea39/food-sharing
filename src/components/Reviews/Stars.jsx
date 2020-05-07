db28164add9a52e4166b2cc67bbb0d114b6e3b0c
Mihai Oprea <mihai.oprea@iquestgroup.com>
2020-05-07 15:12:27 +0300

    Stars tweaks.

--- src/components/Reviews/Stars.jsx
+++ src/components/Reviews/Stars.jsx
@@ -5,39 +5,41 @@ export default function FoodShareStars() {
 
     const onContainerLeave = () => {
         if (!container.current.classList.contains('rated')) {
-            for (const star of container.current.children) {
+            Array.from(container.current.children).forEach((star) => {
                 star.classList.add('text-gray');
                 star.classList.remove('text-warning');
                 star.classList.add('far');
                 star.classList.remove('fas');
-            }
+            });
         }
     };
 
     const onStarEnter = () => {
-        for (const [index, star] of Array.of(container.current.children).entries()) {
+        Array.from(container.current.children).forEach((star, index) => {
             const rating = Number(star.getAttribute('data-rating-value'));
-            console.log('here', star);
-            // if (index + 1 <= rating) {
-            //     star.classList.remove('text-gray');
-            //     star.classList.add('text-warning');
-            // }
-            // if (index >= rating) {
-            //     star.classList.add('text-gray');
-            //     star.classList.remove('text-warning');
-            //     star.classList.remove('fas');
-            //     star.classList.add('far');
-            // }
-        }
+            
+            console.log(rating, index);
+
+            if (index + 1 <= rating) {
+                star.classList.remove('text-gray');
+                star.classList.add('text-warning');
+            }
+            if (index >= rating) {
+                star.classList.add('text-gray');
+                star.classList.remove('text-warning');
+                star.classList.remove('fas');
+                star.classList.add('far');
+            }
+        });
     };
 
     return (
         <div className={`foodshare-stars-container`} onMouseLeave={onContainerLeave} ref={container}>
-            <i className={`foodshare-star star fas fa-star text-gray`} onMouseEnter={onStarEnter} data-rating-value="1"/>
-            <i className={`foodshare-star star fas fa-star text-gray`} onMouseEnter={onStarEnter} data-rating-value="2"/>
-            <i className={`foodshare-star star fas fa-star text-gray`} onMouseEnter={onStarEnter} data-rating-value="3"/>
-            <i className={`foodshare-star star fas fa-star text-gray`} onMouseEnter={onStarEnter} data-rating-value="4"/>
-            <i className={`foodshare-star star fas fa-star text-gray`} onMouseEnter={onStarEnter} data-rating-value="5"/>
+            <i className={`foodshare-star star fas fa-star text-gray`} data-rating-value="1"/>
+            <i className={`foodshare-star star fas fa-star text-gray`} data-rating-value="2"/>
+            <i className={`foodshare-star star fas fa-star text-gray`} data-rating-value="3"/>
+            <i className={`foodshare-star star fas fa-star text-gray`} data-rating-value="4"/>
+            <i className={`foodshare-star star fas fa-star text-gray`} data-rating-value="5"/>
         </div>
     );
 }
